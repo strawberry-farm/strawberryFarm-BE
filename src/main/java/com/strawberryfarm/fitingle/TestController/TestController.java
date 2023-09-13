e481d4107f1247cda7def2f09431d394d4ce6d2d
HyungDong <mansa0805@gmail.com>
2023-09-14 00:06:29 +0900

    request mapping

--- src/main/java/com/strawberryfarm/fitingle/TestController/TestController.java
+++ src/main/java/com/strawberryfarm/fitingle/TestController/TestController.java
@@ -1,13 +1,16 @@
 package com.strawberryfarm.fitingle.TestController;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/test",produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
-	@GetMapping("/test")
+	@GetMapping("")
 	public ResponseEntity<String> TestMethod(){
 		System.out.println("연결성확인");
 		return ResponseEntity.ok("TEST API");
