f0b30dbc7906fd08781e1f4b86a8e726455eedf6
HyungDong <mansa0805@gmail.com>
2023-09-13 23:19:03 +0900

    TEST API ãCONTROLLER -> RESTCONTROLLER

--- src/main/java/com/strawberryfarm/fitingle/TestController/TestController.java
+++ src/main/java/com/strawberryfarm/fitingle/TestController/TestController.java
@@ -3,11 +3,12 @@ package com.strawberryfarm.fitingle.TestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 	@GetMapping("/test")
-	public String TestMethod(){
-		return "testest";
+	public ResponseEntity<String> TestMethod(){
+		return ResponseEntity.ok("TEST API");
 	}
 }
