75253e231a11d40d6affd10e1c8a8194fb1b1113
HyungDong <mansa0805@gmail.com>
2023-09-13 23:56:58 +0900

    TEST API MAINCODE

--- src/main/java/com/strawberryfarm/fitingle/FitingleApplication.java
+++ src/main/java/com/strawberryfarm/fitingle/FitingleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitingleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start fitingle");
 		SpringApplication.run(FitingleApplication.class, args);
 	}
 
