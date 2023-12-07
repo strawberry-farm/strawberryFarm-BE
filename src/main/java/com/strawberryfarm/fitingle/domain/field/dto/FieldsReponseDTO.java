be534354b159fdf45dc37db4b353a80c65e91fa3
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2023-12-07 23:57:56 +0900

    fix: baseDto extends로 수정

--- src/main/java/com/strawberryfarm/fitingle/domain/field/dto/FieldsReponseDTO.java
+++ src/main/java/com/strawberryfarm/fitingle/domain/field/dto/FieldsReponseDTO.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class FieldsReponseDTO implements BaseDto {
+public class FieldsReponseDTO extends BaseDto {
 
     private Long fieldId;
     private String fieldName;
