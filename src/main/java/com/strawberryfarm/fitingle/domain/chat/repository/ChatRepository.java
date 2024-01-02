4483bcb166b0a3a43639200ec74805ec79e4bb1c
HyungDong <mansa0805@gmail.com>
2024-01-02 17:55:48 +0900

    사미완성 코드 삽입으로 인한 에러 í 해결 1

--- src/main/java/com/strawberryfarm/fitingle/domain/chat/repository/ChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.strawberryfarm.fitingle.domain.chat.repository;
-
-import com.strawberryfarm.fitingle.domain.chat.entity.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-
-}
