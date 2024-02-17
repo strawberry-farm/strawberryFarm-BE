72ff1c5b55cf82f60c07b5fe41a048e0ec183d17
서형동(Seo HyungDong) <58661684+GaJaMy@users.noreply.github.com>
2024-02-17 16:21:14 +0900

    쿠키 도메인 삭제.java

--- src/main/java/com/strawberryfarm/fitingle/domain/auth/controller/AuthController.java
+++ src/main/java/com/strawberryfarm/fitingle/domain/auth/controller/AuthController.java
@@ -149,7 +149,6 @@ public class AuthController {
 			.sameSite("None")
 			.httpOnly(true)
 			.secure(true)
-			.domain("localhost")
 			.maxAge(Duration.ofDays(1))
 			.build();
 		response.addHeader("Set-Cookie",cookie.toString());
